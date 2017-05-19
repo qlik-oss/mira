d78d98282702804fce268de4708c41368e483c51
Jan Miderbäck <jan.miderback@qlik.com>
2017-05-19 11:20:45 +0200

    Config tests updated + npm test

--- src/Config.js
+++ src/Config.js
@@ -11,19 +11,35 @@ const logger = require('./logger/Logger').get();
  */
 class Config {
   /**
+   * The port Mira exposes its REST API on.
+   */
+  static get miraPort() { return 9100; }
+
+  /**
+   * The port Mira uses to communicate to QIX Engine instances in a container-to-container configuration.
+   */
+  static get enginePort() { return 9076; }
+
+  /**
+   * The Docker image name that Mira uses to identify QIX Engine instances.
+   */
+  static get engineImageName() {
+    if (!process.env.QIX_ENGINE_IMAGE_NAME) {
+      return 'qlikea/engine';
+    }
+    return process.env.QIX_ENGINE_IMAGE_NAME;
+  }
+
+  /**
    * Initializes the configuration.
    * @param {object} commandLineOptions - Options passed as command line arguments when starting the service.
    */
   static init(commandLineOptions) {
-    Config.miraPort = 9100;
-    Config.enginePort = 9076;
-
-    Config.engineImageName = process.env.QIX_ENGINE_IMAGE_NAME;
-    if (!Config.engineImageName) {
-      Config.engineImageName = 'qlikea/engine';
+    if (!commandLineOptions) {
+      Config.mode = 'swarm';
+    } else {
+      Config.mode = commandLineOptions.mode || 'swarm';
     }
-
-    Config.mode = commandLineOptions.mode || 'swarm';
     if (Config.mode !== 'local' && Config.mode !== 'swarm') {
       logger.error('Incorrect operation mode. Use --mode option.');
       process.exit(1);
---
+++ test/unit/Config.spec.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-unused-expressions */
+const Config = require('../../src/Config');
+const chai = require('chai');
+
+describe('Config', () => {
+  describe('#miraPort', () => {
+    it('should have correct value', () => {
+      const expectedMiraPort = 9100;
+      chai.expect(Config.miraPort).to.equal(expectedMiraPort);
+    });
+  });
+
+  describe('#enginePort', () => {
+    it('should have correct value', () => {
+      const expectedEnginePort = 9076;
+      chai.expect(Config.enginePort).to.equal(expectedEnginePort);
+    });
+  });
+
+  describe('#engineImageName', () => {
+    it('should have correct value', () => {
+      const expectedEngineImageName = 'qlikea/engine';
+      chai.expect(Config.engineImageName).to.equal(expectedEngineImageName);
+    });
+  });
+
+  describe('#mode', () => {
+    it('should be undefined when not initialized', () => {
+      chai.expect(Config.mode).to.be.undefined;
+    });
+    it('should return correct default value after initialization', () => {
+      Config.init();
+      chai.expect(Config.mode).to.equal('swarm');
+      Config.init({});
+      chai.expect(Config.mode).to.equal('swarm');
+    });
+    it('should return same value as provided in initialization', () => {
+      Config.init({ mode: 'local' });
+      chai.expect(Config.mode).to.equal('local');
+      Config.init({ mode: 'swarm' });
+      chai.expect(Config.mode).to.equal('swarm');
+    });
+  });
+});
