c706d08641d148f26eb92e0b37b57001350d13df
Jan Miderbäck <jan.miderback@qlik.com>
2017-05-19 13:39:30 +0200

    Don't require('chai')

--- test/unit/Config.spec.js
+++ test/unit/Config.spec.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-expressions */
 const Config = require('../../src/Config');
-const chai = require('chai');
 
 describe('Config', () => {
   before(() => {
@@ -10,30 +9,30 @@ describe('Config', () => {
   describe('#miraPort', () => {
     it('should have correct value', () => {
       const expectedMiraPort = 9100;
-      chai.expect(Config.miraPort).to.equal(expectedMiraPort);
+      expect(Config.miraPort).to.equal(expectedMiraPort);
     });
   });
 
   describe('#enginePort', () => {
     it('should have correct value', () => {
       const expectedEnginePort = 9076;
-      chai.expect(Config.enginePort).to.equal(expectedEnginePort);
+      expect(Config.enginePort).to.equal(expectedEnginePort);
     });
   });
 
   describe('#engineImageName', () => {
     it('should have correct value', () => {
       const expectedEngineImageName = 'qlikea/engine';
-      chai.expect(Config.engineImageName).to.equal(expectedEngineImageName);
+      expect(Config.engineImageName).to.equal(expectedEngineImageName);
     });
   });
 
   describe('#mode', () => {
     it('should return correct default value after initialization', () => {
       Config.init();
-      chai.expect(Config.mode).to.equal('swarm');
+      expect(Config.mode).to.equal('swarm');
       Config.init({});
-      chai.expect(Config.mode).to.equal('swarm');
+      expect(Config.mode).to.equal('swarm');
     });
     it('should return same value as provided in initialization', () => {
       Config.init({ mode: 'local' });
