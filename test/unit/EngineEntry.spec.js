293b83b007679f3a2b70e87b71a51790901626c0
Marcus Offesson <marcusoffesson@users.noreply.github.com>
2017-06-27 08:00:29 +0200

    EngineEntry::satisfies unit tests

--- test/unit/EngineEntry.spec.js
+++ test/unit/EngineEntry.spec.js
@@ -61,4 +61,59 @@ describe('EngineEntry', () => {
       entry.stopHealthChecks();
     });
   });
+
+  describe('#satisfies', () => {
+    it('should handle non existing keys', () => {
+      expect(entry.satisfies({ c: '> 150' })).to.be.false;
+    });
+
+    it('should handle arrays', () => {
+      entry.properties.numb = 23;
+      expect(entry.satisfies({ a: ['dummy', 'foo'] })).to.be.true;
+      expect(entry.satisfies({ a: ['dummy', 'bar'] })).to.be.false;
+      expect(entry.satisfies({ numb: [21, 22, 23] })).to.be.true;
+      expect(entry.satisfies({ numb: ['21', '22', '23'] })).to.be.false;
+    });
+
+    it('should handle boolean types', () => {
+      entry.properties.bool = false;
+      expect(entry.satisfies({ bool: false })).to.be.true;
+    });
+
+    it('should handle a mix of boolean and string types', () => {
+      entry.properties.bool1 = false;
+      entry.properties.bool2 = 'false';
+      entry.properties.bool3 = 'FaLsE';
+      expect(entry.satisfies({ bool1: 'false' })).to.be.true;
+      expect(entry.satisfies({ bool1: 'FaLsE' })).to.be.true;
+      expect(entry.satisfies({ bool2: false })).to.be.true;
+      expect(entry.satisfies({ bool3: false })).to.be.true;
+    });
+
+    it('should handle greater than', () => {
+      entry.properties.numb = 30;
+      expect(entry.satisfies({ numb: '>29' })).to.be.true;
+      expect(entry.satisfies({ numb: '>30' })).to.be.false;
+      expect(entry.satisfies({ numb: '>31' })).to.be.false;
+    });
+
+    it('should handle less than', () => {
+      entry.properties.numb = 30;
+      expect(entry.satisfies({ numb: '<29' })).to.be.false;
+      expect(entry.satisfies({ numb: '<30' })).to.be.false;
+      expect(entry.satisfies({ numb: '<31' })).to.be.true;
+    });
+
+    it('should handle equal values', () => {
+      entry.properties.numb = 30;
+      entry.properties.str = 'foobar';
+      expect(entry.satisfies({ numb: 30 })).to.be.true;
+      expect(entry.satisfies({ numb: 31 })).to.be.false;
+      expect(entry.satisfies({ numb: '30' })).to.be.true;
+      expect(entry.satisfies({ numb: '31' })).to.be.false;
+      expect(entry.satisfies({ str: 'foobar' })).to.be.true;
+      expect(entry.satisfies({ str: 'barfoo' })).to.be.false;
+      expect(entry.satisfies({ str: 123 })).to.be.false;
+    });
+  });
 });
