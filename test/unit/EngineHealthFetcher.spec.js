bf0f46a8d01db4c6f76092fa0ce79516c46097b3
Jan Miderbäck <jan.miderback@qlik.com>
2017-08-16 14:27:07 +0200

    Initial commit

---
+++ test/unit/EngineHealthFetcher.spec.js
@@ -0,0 +1,56 @@
+// const EngineHealthFetcher = require('../../src/EngineHealthFetcher');
+// const sleep = require('../test-utils/sleep');
+
+describe('EngineHealthFetcher', () => {
+  // let FakeDockerClient;
+
+  beforeEach(() => {
+  });
+
+  describe('#todo()', () => {
+    it('todo', async () => {
+      // const listEnginesStub = sinon.stub(FakeDockerClient, 'listEngines', () => []);
+      // const engineDiscovery = new EngineDiscovery(FakeDockerClient, 20, 5000);
+      // await sleep(50);
+      // expect(engineDiscovery).to.not.be.null;
+      // expect(engineDiscovery).to.not.be.undefined;
+      // expect(listEnginesStub.callCount >= 2).to.be.true;
+    });
+  });
+
+  describe('#todo()', () => {
+    // const port = 9076;
+    // const engine1 = { key: 'e1', ipAddress: '10.0.0.1', port, properties: { p1: 'foo', p2: 'bar' } };
+    // const engine2 = { key: 'e2', ipAddress: '10.0.0.2', port, properties: { p1: 'abc', p2: 'bar' } };
+    // const engine3 = { key: 'e3', ipAddress: '10.0.0.3', port, properties: { p1: 'foo', p2: 'xyz' } };
+    // const engines1 = [engine1, engine2];
+    // const engines2 = [engine2, engine3];
+
+    it('todo', async () => {
+      // let listEnginesStub = sinon.stub(FakeDockerClient, 'listEngines', () => engines1);
+      // const engineDiscovery = new EngineDiscovery(FakeDockerClient, 20, 100000);
+      // await sleep(50);
+      // let listedEngines = await engineDiscovery.list();
+
+      // expect(listedEngines.length).to.equal(2);
+      // let listedEngine1 = listedEngines[0];
+      // let listedEngine2 = listedEngines[1];
+      // expect(listedEngine1.properties).to.deep.equal(engine1.properties);
+      // expect(listedEngine2.properties).to.deep.equal(engine2.properties);
+
+      // listEnginesStub.restore();
+      // listEnginesStub = sinon.stub(FakeDockerClient, 'listEngines', () => engines2);
+      // await sleep(50);
+      // listedEngines = await engineDiscovery.list();
+
+      // expect(listedEngines.length).to.equal(2);
+      // listedEngine1 = listedEngines[0];
+      // listedEngine2 = listedEngines[1];
+      // expect(listedEngine1.properties).to.deep.equal(engine2.properties);
+      // expect(listedEngine2.properties).to.deep.equal(engine3.properties);
+    });
+
+    it('todo', async () => {
+    });
+  });
+});
