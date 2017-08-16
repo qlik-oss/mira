a1209dd8ef5b3b88c1ce2689645dd0499a6f3837
Jan Miderbäck <jan.miderback@gmail.com>
2017-08-16 12:07:43 +0200

    Added LocalDockerClient unit test. (#75)
    Closes #65

--- src/docker/LocalDockerClient.js
+++ src/docker/LocalDockerClient.js
@@ -3,7 +3,8 @@ const containerized = require('containerized');
 const logger = require('../logger/Logger').get();
 
 const localhostIp = '127.0.0.1';
-const docker = new Docker();
+
+let dockerode = new Docker();
 
 function getProperties(container) {
   return Object.assign({}, container.Labels);
@@ -30,6 +31,20 @@ function getPort(container) {
  */
 class LocalDockerClient {
   /**
+   * Gets the Dockerode instance used.
+   * Mainly for testing purposes. Should normally not be used externally.
+   * @returns {Docker} The Dockerode instance used for Docker Engine API access.
+   */
+  static get docker() { return dockerode; }
+
+  /**
+   * Sets the Dockerode instance to use.
+   * Mainly for testing purposes. Should normally not be used externally,
+   * @param {Docker} value - The Dockerode instance to use for Docker Engine API access.
+   */
+  static set docker(value) { dockerode = value; }
+
+  /**
    * Lists engines.
    * @param {string} engineImageName - The Engine Docker image name used to determine if a
    *   container is an engine instance.
@@ -37,7 +52,7 @@ class LocalDockerClient {
    */
   static async listEngines(engineImageName) {
     return new Promise((resolve, reject) => {
-      docker.listContainers((err, containers) => {
+      LocalDockerClient.docker.listContainers((err, containers) => {
         if (!err) {
           const engineContainers = containers.filter(
             container => (container.Image.indexOf(engineImageName) === 0)
---
+++ test/unit/docker/LocalDockerClient.spec.js
@@ -0,0 +1,38 @@
+const Docker = require('dockerode');
+const DockerClient = require('../../../src/docker/LocalDockerClient');
+const specData = require('./LocalDockerClient.spec.data.json');
+
+const docker = new Docker();
+
+describe('LocalDockerClient', () => {
+  let listContainersStub;
+
+  before(() => {
+    listContainersStub = sinon.stub(docker, 'listContainers', callback => callback(undefined, specData.endpointsResponse));
+    // Provide stubbed Dockerode instance for testing purposes.
+    DockerClient.docker = docker;
+  });
+
+  afterEach(() => {
+    listContainersStub.reset();
+  });
+
+  describe('#listEngines', () => {
+    it('should list two engines with matching image name', async () => {
+      const engines = await DockerClient.listEngines('qlikea/engine');
+      const rawEngines = engines.map(engine => ({
+        properties: engine.properties,
+        ipAddress: engine.ipAddress,
+        port: engine.port,
+      }));
+      expect(listContainersStub).to.be.called.once;
+      expect(rawEngines).to.deep.equal(specData.miraOutput);
+    });
+
+    it('should not list any engines since no container matches provided image name', async () => {
+      const engines = await DockerClient.listEngines('xxxyyyzzz');
+      expect(listContainersStub).to.be.called.once;
+      expect(engines.length === 0).to.be.true;
+    });
+  });
+});
