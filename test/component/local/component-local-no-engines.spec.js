48baf27a8e34d488804630f3928fce6ba85ebb07
Mattias Wennmo <mow@qlik.com>
2017-08-25 08:59:38 +0200

    Fix lint errors

--- test/component/local/component-local-no-engines.spec.js
+++ test/component/local/component-local-no-engines.spec.js
@@ -6,12 +6,15 @@ process.env.DOCKER_HOST = 'http://localhost:8001';
 
 chai.use(chaiHttp);
 
-describe('Mira in local docker mode', () => {
+describe('Mira in local docker mode with no engines', () => {
   let server;
 
-  it('with no engines should return an empty engine list', async () => {
+  before(() => {
     nock('http://localhost:8001').get('/containers/json').times(10).reply(200, []);
     server = require('../../../src/index'); // eslint-disable-line global-require
+  });
+
+  it('should return an empty engine list', async () => {
     const res = await chai.request(miraEndpoint).get('/v1/engines');
     expect(res).to.be.json;
     expect(res.body.length).to.equal(0);
--- test/component/local/component-local-two-engines.spec.js
+++ test/component/local/component-local-two-engines.spec.js
@@ -7,18 +7,18 @@ process.env.DOCKER_HOST = 'http://localhost:8001';
 
 chai.use(chaiHttp);
 
-describe('Mira in local docker mode', () => {
+describe('Mira in local docker mode with two engines', () => {
   let server;
 
-  describe('with two engines', () => {
-    
-    it('should return a list with two engines', async () => {
-      nock('http://localhost:8001').get('/containers/json').times(10).reply(200, specDataLocal.endpointsResponse);
-      server = require('../../../src/index'); // eslint-disable-line global-require
-      const res = await chai.request(miraEndpoint).get('/v1/engines');
-      expect(res).to.be.json;
-      expect(res.body.length).to.equal(2);
-    });
+  before(() => {
+    nock('http://localhost:8001').get('/containers/json').times(10).reply(200, specDataLocal.endpointsResponse);
+    server = require('../../../src/index'); // eslint-disable-line global-require
+  });
+
+  it('should return a list with two engines', async () => {
+    const res = await chai.request(miraEndpoint).get('/v1/engines');
+    expect(res).to.be.json;
+    expect(res.body.length).to.equal(2);
   });
 
   after(() => {
