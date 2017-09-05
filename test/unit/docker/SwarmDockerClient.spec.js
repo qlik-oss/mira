2eef0795fb8041e87439e0a4cf6850f7ce137340
Mattias Wennmo <mow@qlik.com>
2017-09-05 15:17:41 +0200

    Update implementation and tests for swarm mode

--- test/unit/docker/SwarmDockerClient.spec.js
+++ test/unit/docker/SwarmDockerClient.spec.js
@@ -16,10 +16,8 @@ describe('SwarmDockerClient', () => {
       listTasksStub = sinon.stub(docker, 'listTasks', (opts, callback) => callback(undefined, specData.endpointsResponse));
       DockerClient.docker = docker;
       const engines = await DockerClient.listEngines('qix-engine');
-      const rawEngines = engines.map(engine => ({
-        properties: engine.properties,
-        ipAddress: engine.ipAddress,
-        port: engine.port,
+      const rawEngines = engines.map(item => ({
+        engine: item.engine,
       }));
       expect(listTasksStub).to.be.called.once;
       expect(rawEngines).to.deep.equal(specData.miraOutput);
@@ -31,6 +29,24 @@ describe('SwarmDockerClient', () => {
       const engines = await DockerClient.listEngines('xxxyyyzzz');
       expect(listTasksStub).to.be.called.once;
       expect(engines.length === 0).to.be.true;
+    });
+
+    it('the swarm property should be set and hold the container info', async () => {
+      listTasksStub = sinon.stub(docker, 'listTasks', (opts, callback) => callback(undefined, specData.endpointsResponse));
+      const engines = await DockerClient.listEngines('qix-engine');
+      expect(listTasksStub).to.be.called.once;
+      expect(engines[0].swarm).to.deep.equal(specData.endpointsResponse[0]);
+      expect(engines[1].swarm).to.deep.equal(specData.endpointsResponse[1]);
+    });
+
+    it('and local and kubernetes properties should not be set', async () => {
+      listTasksStub = sinon.stub(docker, 'listTasks', (opts, callback) => callback(undefined, specData.endpointsResponse));
+      const engines = await DockerClient.listEngines('qix-engine');
+      expect(listTasksStub).to.be.called.once;
+      expect(engines[0].local).to.be.undefined;
+      expect(engines[0].kubernetes).to.be.undefined;
+      expect(engines[1].local).to.be.undefined;
+      expect(engines[1].kubernetes).to.be.undefined;
     });
   });
 });
