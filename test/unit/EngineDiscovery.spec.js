495342268a2f0f7d0fb3ed31c5f1dc78645d956e
Mattias Wennmo <wennmo@users.noreply.github.com>
2017-09-05 14:54:55 +0200

    WIP Adapt implementation of local docker mode to updated API (#122)
    * Update property structure for local docker
    
    * Remove unset properties
    
    * Updates according to comments
    
    * Fix incorrect unit test case for EngineDiscovery

--- test/unit/EngineDiscovery.spec.js
+++ test/unit/EngineDiscovery.spec.js
@@ -21,13 +21,13 @@ describe('EngineDiscovery', () => {
 
   describe('#list()', () => {
     const port = 9076;
-    const engine1 = { key: 'e1', ipAddress: '10.0.0.1', port, properties: { p1: 'foo', p2: 'bar' } };
-    const engine2 = { key: 'e2', ipAddress: '10.0.0.2', port, properties: { p1: 'abc', p2: 'bar' } };
-    const engine3 = { key: 'e3', ipAddress: '10.0.0.3', port, properties: { p1: 'foo', p2: 'xyz' } };
+    const engine1 = { key: 'e1', engine: { ip: '10.0.0.1', port } };
+    const engine2 = { key: 'e2', engine: { ip: '10.0.0.2', port } };
+    const engine3 = { key: 'e3', engine: { ip: '10.0.0.3', port } };
     const engines1 = [engine1, engine2];
     const engines2 = [engine2, engine3];
 
-    it('should list all discovered engines when no property filter passed', async () => {
+    it('should list all discovered engines', async () => {
       let listEnginesStub = sinon.stub(FakeDockerClient, 'listEngines', () => engines1);
       const engineDiscovery = new EngineDiscovery(FakeDockerClient, 20, 100000);
       await sleep(50);
@@ -36,8 +36,8 @@ describe('EngineDiscovery', () => {
       expect(listedEngines.length).to.equal(2);
       let listedEngine1 = listedEngines[0];
       let listedEngine2 = listedEngines[1];
-      expect(listedEngine1.properties).to.deep.equal(engine1.properties);
-      expect(listedEngine2.properties).to.deep.equal(engine2.properties);
+      expect(listedEngine1.engine).to.deep.equal(engine1.engine);
+      expect(listedEngine2.engine).to.deep.equal(engine2.engine);
 
       listEnginesStub.restore();
       listEnginesStub = sinon.stub(FakeDockerClient, 'listEngines', () => engines2);
@@ -47,30 +47,8 @@ describe('EngineDiscovery', () => {
       expect(listedEngines.length).to.equal(2);
       listedEngine1 = listedEngines[0];
       listedEngine2 = listedEngines[1];
-      expect(listedEngine1.properties).to.deep.equal(engine2.properties);
-      expect(listedEngine2.properties).to.deep.equal(engine3.properties);
-    });
-
-    it('should list discovered engines matching passed filter', async () => {
-      let listEnginesStub = sinon.stub(FakeDockerClient, 'listEngines', () => engines1);
-      const engineDiscovery = new EngineDiscovery(FakeDockerClient, 20, 100000);
-      await sleep(50);
-      let listedEngines = await engineDiscovery.list({ p1: 'foo' });
-
-      expect(listedEngines.length).to.equal(1);
-      let listedEngine = listedEngines[0];
-      expect(listedEngine.properties.p1).to.equal('foo');
-      expect(listedEngine.properties.p2).to.equal('bar');
-
-      listEnginesStub.restore();
-      listEnginesStub = sinon.stub(FakeDockerClient, 'listEngines', () => engines2);
-      await sleep(50);
-      listedEngines = await engineDiscovery.list({ p1: 'foo' });
-
-      expect(listedEngines.length).to.equal(1);
-      listedEngine = listedEngines[0];
-      expect(listedEngine.properties.p1).to.equal('foo');
-      expect(listedEngine.properties.p2).to.equal('xyz');
+      expect(listedEngine1.engine).to.deep.equal(engine2.engine);
+      expect(listedEngine2.engine).to.deep.equal(engine3.engine);
     });
   });
 });
