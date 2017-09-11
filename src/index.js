b2625cc1e978bfc2f12e9ed2f8a673a9f569a4fb
Mattias Wennmo <mow@qlik.com>
2017-09-11 14:57:22 +0200

    Generate api-spec from jsdoc

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const koaLoggerWinston = require('koa-logger-winston');
-const swagger = require('swagger2');
 const swagger2koa = require('swagger2-koa');
-const path = require('path');
+const swaggerJSDoc = require('swagger-jsdoc');
 const logger = require('./logger/Logger').get();
 const Config = require('./Config');
 const EngineDiscovery = require('./EngineDiscovery');
 const getDockerClient = require('./docker/getDockerClient');
+const fs = require('fs');
 
 const apiVersion = 'v1';
 const healthEndpoint = 'health';
@@ -22,12 +22,35 @@ const engineDiscovery = new EngineDiscovery(
   DockerClient,
   Config.engineDiscoveryRefreshRate,
   Config.engineHealthRefreshRate);
-const document = swagger.loadDocumentSync(path.join(__dirname, './../doc/api-doc.yml'));
 
 function onUnhandledError(err) {
   logger.error('Process encountered an unhandled error', err);
   process.exit(1);
 }
+
+// Swagger definition
+const swaggerDefinition = {
+  info: {
+    title: 'A QIX Engine discovery service for Qlik Elastic',
+    version: '0.0.1',
+    description: 'REST API for discovering QIX Engines running in Docker containers. Different modes exist, local mode, swarm and kubernetes mode.',
+  },
+  host: 'localhost:9100',
+  schemes: ['http'],
+  basePath: '/v1',
+  'x-qlik-visibility': 'private',
+  'x-qlik-stability': 'experimental',
+};
+
+// Options for the swagger docs
+const options = {
+  swaggerDefinition,
+  apis: ['./src/index.js'],
+};
+
+// Initialize swagger-jsdoc -> returns validated swagger spec in json format
+const swaggerSpec = swaggerJSDoc(options);
+fs.writeFileSync('./doc/api-doc.json', JSON.stringify(swaggerSpec));
 
 /*
  * Service bootstrapping
@@ -43,12 +66,36 @@ process.on('SIGTERM', () => {
 process.on('uncaughtException', onUnhandledError);
 process.on('unhandledRejection', onUnhandledError);
 
+/**
+  * @swagger
+  * /health:
+  *   get:
+  *     description: Returns health status of the Mira service
+  *     responses:
+  *       200:
+  *         description: OK
+  */
 router.get(`/${healthEndpoint}`, async (ctx) => { ctx.body = 'OK'; });
 
+/**
+  * @swagger
+  * /engines:
+  *   get:
+  *     description:  Lists available QIX Engines.
+  *     produces:
+  *       - application/json
+  *     responses:
+  *       200:
+  *         description: successful operation
+  *         schema:
+  *           type: array
+  *           items:
+  *             $ref: '#/definitions/containerInfo'
+  */
 router.get(`/${enginesEndpoint}`, async (ctx) => { ctx.body = await engineDiscovery.list(); });
 
 app
-  .use(swagger2koa.ui(document, '/openapi'))
+  .use(swagger2koa.ui(swaggerSpec, '/openapi'))
   .use(koaLoggerWinston(logger))
   .use(router.routes())
   .use(router.allowedMethods());
@@ -58,3 +105,50 @@ const server = app.listen(Config.miraApiPort);
 logger.info(`Listening on port ${Config.miraApiPort}`);
 
 module.exports = server;
+
+/**
+ * @swagger
+ * definitions:
+ *   engineInfo:
+ *     type: object
+ *     properties:
+ *       ip:
+ *         description: IP address to use when connecting to the QIX Engine.
+ *         type: string
+ *       port:
+ *         description: Port to use when communicating with the QIX Engine API.
+ *         type: number
+ *       metricsPort:
+ *         description: Port to use when retrieving the QIX Engine metrics.
+ *         type: number
+ *       status:
+ *         $ref: '#/definitions/containerStatus'
+ *       health:
+ *          description: Last health endpoint response of the QIX Engine.
+ *          type: object
+ *       metrics:
+ *          description: Last metrics endpoint response of the QIX Engine.
+ *   containerInfo:
+ *     type: object
+ *     required:
+ *      - engine
+ *     properties:
+ *       engine:
+ *         $ref: '#/definitions/engineInfo'
+ *       local:
+ *         type: object
+ *         description: Entire container response if running in local docker mode.
+ *       swarm:
+ *         type: object
+ *         description: Entire container response if running in docker swarm mode.
+ *       kubernetes:
+ *         type: object
+ *         description: Entire container response if running in kubernetes mode.
+ *   containerStatus:
+ *     type: string
+ *     description: Status of the QIX Engine.
+ *     enum:
+ *       - OK
+ *       - UNHEALTHY
+ *       - NO_METRICS
+ */
