d22e1f4c795c8f8b2e7727118deec141455340a4
Mattias Wennmo <mow@qlik.com>
2017-09-15 09:14:44 +0200

    Create npm task for generating swagger spec

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const Koa = require('koa');
 const koaLoggerWinston = require('koa-logger-winston');
+const swagger = require('swagger2');
 const swagger2koa = require('swagger2-koa');
-const swaggerJSDoc = require('swagger-jsdoc');
+const path = require('path');
 const logger = require('./logger/Logger').get();
-const fs = require('fs-extra');
 const Config = require('./Config');
 
 Config.init();
@@ -12,34 +12,12 @@ const router = require('./Routes');
 
 const app = new Koa();
 
+const document = swagger.loadDocumentSync(path.join(__dirname, './../doc/api-doc.yml'));
+
 function onUnhandledError(err) {
   logger.error('Process encountered an unhandled error', err);
   process.exit(1);
 }
-
-// Swagger definition
-const swaggerDefinition = {
-  info: {
-    title: 'A QIX Engine discovery service for Qlik Elastic',
-    version: '0.0.1',
-    description: 'REST API for discovering QIX Engines running in Docker containers. Different modes exist, local, swarm and kubernetes mode.',
-  },
-  host: 'localhost:9100',
-  schemes: ['http'],
-  basePath: '/v1',
-  'x-qlik-visibility': 'private',
-  'x-qlik-stability': 'experimental',
-};
-
-// Options for the swagger docs
-const options = {
-  swaggerDefinition,
-  apis: ['./src/index.js', './src/Routes.js'],
-};
-
-// Initialize swagger-jsdoc -> returns validated swagger spec in json format
-const swaggerSpec = swaggerJSDoc(options);
-fs.ensureDir('./doc').then(() => fs.writeJsonSync('./doc/api-doc.json', swaggerSpec));
 
 /*
  * Service bootstrapping
@@ -56,7 +34,7 @@ process.on('uncaughtException', onUnhandledError);
 process.on('unhandledRejection', onUnhandledError);
 
 app
-  .use(swagger2koa.ui(swaggerSpec, '/openapi'))
+  .use(swagger2koa.ui(document, '/openapi'))
   .use(koaLoggerWinston(logger))
   .use(router.routes())
   .use(router.allowedMethods());
