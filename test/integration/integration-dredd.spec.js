38df5295322bd7d60ff4f4b7ec4fc89d1f2cfe8d
Mattias Wennmo <mow@qlik.com>
2017-09-22 09:21:44 +0200

    Add assertions for dredd

--- test/integration/integration-dredd.spec.js
+++ test/integration/integration-dredd.spec.js
@@ -16,6 +16,8 @@ describe('Validating the OpenAPI spec using Dredd', () => {
     const dredd = new Dredd(dreddConfiguration);
     dredd.run((error, stats) => {
       expect(error).to.be.null;
+      expect(stats.tests).to.be.greaterThan(0);
+      expect(stats.passes).to.equal(stats.tests);
       expect(stats.errors).to.equal(0);
       expect(stats.failures).to.equal(0);
       done();
