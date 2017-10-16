954b4c0f27ee22910d94e5cfc0e0be042b2648f5
Jan Miderbäck <jan.miderback@gmail.com>
2017-10-16 16:20:18 +0200

    Release script based on qlik/cloud-releaser (#160)
    Fixes #148

---
+++ release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Usage:
+#   release.sh [major | minor | patch]
+#
+#   If no argument is passed, it defaults to patch.
+#
+
+RELEASE_TYPE=$1
+docker run -e RELEASE_TYPE -e REPO=qlik-ea/mira -e GITHUB_API_TOKEN -v /$HOME/.gitconfig://root/.gitconfig:ro qlik/cloud-releaser:latest
