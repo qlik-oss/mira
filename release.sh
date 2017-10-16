c81fc1baebb299fb211ca3fde87b0d8dc01bf70f
Jan Miderbäck <jan.miderback@qlik.com>
2017-10-16 13:54:05 +0200

    Setting version to 0.0.3-0 [ci skip]

--- release.sh
+++ release.sh
@@ -6,4 +6,4 @@
 #
 
 RELEASE_TYPE=$1
-docker run -e RELEASE_TYPE -e REPO=qlik-ea/mira -e GITHUB_API_TOKEN -v $HOME/.gitconfig:/root/.gitconfig:ro qlik/cloud-releaser:latest
+docker run -e RELEASE_TYPE -e REPO=qlik-ea/mira -e GITHUB_API_TOKEN -v /$HOME/.gitconfig://root/.gitconfig:ro qlik/cloud-releaser:latest
