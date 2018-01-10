816461fcf476e834387cb7ac38e5dee26e645587
Mattias Wennmo <mow@qlik.com>
2018-01-10 15:31:04 +0100

    Change default discovery interval and adapt tests

--- test/component/local/component-local-no-engines.spec.js
+++ test/component/local/component-local-no-engines.spec.js
@@ -3,6 +3,8 @@ const request = require('supertest');
 const sleep = require('../../test-utils/sleep');
 
 process.env.DOCKER_HOST = 'http://localhost:8001';
+process.env.MIRA_ENGINE_DISCOVERY_INTERVAL = 1000;
+process.env.MIRA_ENGINE_UPDATE_INTERVAL = 1000;
 
 describe('Mira in local docker mode with no engines', () => {
   let app;
