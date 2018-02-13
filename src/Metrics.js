b8bab4bbafb5f7c99722a6a64d67ef94e4c02bc6
Mattias Wennmo <mow@qlik.com>
2018-02-13 12:44:30 +0100

    Add summary http_requests_duration_seconds_total

--- src/Metrics.js
+++ src/Metrics.js
@@ -23,12 +23,16 @@ new prom.Gauge({
 const collectDefaultMetrics = prom.collectDefaultMetrics;
 collectDefaultMetrics();
 
-// Create metric histogram for api response times
-
+// Create metric histogram and summary for api response times
 const responseTimeHistogram = new prom.Histogram({
   name: 'http_request_duration_seconds',
   help: `Time in seconds consumed from ${version.name} receiving a request until a response is sent per path`,
   labelNames: ['path'],
+});
+
+const responseTimeSummary = new prom.Summary({
+  name: 'http_request_duration_seconds_total',
+  help: `Time in seconds consumed from ${version.name} receiving a request until a response is sent in total`,
 });
 
 // function for recording time consumed for a request and adding as metric
@@ -37,6 +41,7 @@ function recordResponseTimes() {
     const requestTime = Date.now();
     await next();
     const diff = Math.ceil((Date.now() - requestTime) / 1000);
+    responseTimeSummary.observe(diff);
     responseTimeHistogram.observe({ path: ctx.request.url }, diff);
     if (diff > Config.allowedResponseTime) {
       logger.warn(`Request for endpoint ${ctx.request.url} took ${diff} s, which is longer than allowed ${Config.allowedResponseTime} s`);
