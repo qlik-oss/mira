aab76a79039101f9278673569ae97308c2d7cdb3
Fredrik Folkesson <Fredrik.Folkesson@qlik.com>
2018-06-04 10:28:16 +0200

    only bumping minor

--- SwaggerDef.js
+++ SwaggerDef.js
@@ -2,7 +2,7 @@
 module.exports = {
   info: {
     title: 'A Qlik Associative Engine discovery service',
-    version: '0.2.2',
+    version: '0.2.0',
     description: 'REST API for discovering Qlik Associative Engines running in Docker containers.',
   },
   host: 'localhost:9100',
