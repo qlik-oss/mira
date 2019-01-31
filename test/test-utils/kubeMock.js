991b56cecfab01708ff3348c528befd5590f0a92
Louis Cloutier <louis.cloutier@qlik.com>
2019-01-31 08:12:42 -0500

    Mock new functions used
    Signed-off-by: Louis Cloutier <louis.cloutier@qlik.com>

--- test/test-utils/kubeMock.js
+++ test/test-utils/kubeMock.js
@@ -18,11 +18,23 @@ module.exports = {
             const res = await request('http://localhost:8001').get('/apis/apps/v1/deployments');
             return { body: res.body };
           },
+          listNamespacedReplicaSet: async () => {
+            const res = await request('http://localhost:8001').get('/apis/apps/v1/namespaces/my-namespace/replicasets');
+            return { body: res.body };
+          },
+          listNamespacedDeployment: async () => {
+            const res = await request('http://localhost:8001').get('/apis/apps/v1/deployments');
+            return { body: res.body };
+          },
         };
       }
       return {
         listPodForAllNamespaces: async () => {
           const res = await request('http://localhost:8001').get('/api/v1/pods?labelSelector=qix-engine');
+          return { body: res.body };
+        },
+        listNamespacedPod: async () => {
+          const res = await request('http://localhost:8001').get('/api/v1/namespaces/my-namespace/pods?labelSelector=qix-engine');
           return { body: res.body };
         },
       };
